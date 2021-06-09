04efb7e7849650dd1f96f761305eea09714b5701
Barry Allen <babanov-artem@yandex.ru>
2021-06-09 18:10:47 +0600

    rebuilding end detection

--- Scripts/Game/gameMain_script.gd
+++ Scripts/Game/gameMain_script.gd
@@ -1,7 +1,19 @@
 extends Node
 
+const COLOR_TEAM_ONE = Color("6699ff")
+const COLOR_TEAM_TWO = Color("ff3399")
+const COLOR_ERROR = Color("ff0000")
+const COLOR_CLEAR = Color("ffffff")
+const FIELD_BORDERS = Vector2(1360,1058)
+const FIELD_START_POINT = Vector2(320,18)
+
+const PRINT_COLLIDERS_INFO = false
+
+enum GameStages {MENU, GENERATING, TESTING, TESTED, PLACING, END}
+
+var gameStage = GameStages.MENU
+
 var hideCursorSquare = true #defines do you need to show Square for Cursor or not 
-var gameStarted = false #defines are you playing rn or using menu
 var cursorSquare #square for cursor
 var currentSquareToPlace #that square that player control rn and want to place
 var whoPlays = 0 #defines the team who plays
@@ -9,14 +21,44 @@ var lastMousePosition = Vector2(320,18)
 var howManySquaresColliding = 0
 var collidingPlayersStartPoint = 0
 var bordersCollidingWithPlayer = {"player1": 0, "player2": 0}
-const COLOR_TEAM_ONE = Color("6699ff")
-const COLOR_TEAM_TWO = Color("ff3399")
-const COLOR_ERROR = Color("ff0000")
-const COLOR_CLEAR = Color("ffffff")
 var rotationFixVector = Vector2(0,0)
+var testingObject = {
+	"Square": null,
+	"Rotated": 0,
+	"SquareSize": Vector2(0,0),
+	"MinSize": 0, #when one size is bigger than another
+	"BiggerSize": 0
+}
+
+var gridSystem = {
+	"grid": [],
+	"playerOneMask": [],
+	"playerTwoMask": []
+}
+
+#var isItTheEnd = false
+
+var gameTime=0
 
 func _ready():
+	print("GameStage = ", gameStage)
+	
+	gridSystem.grid = init_array(gridSystem.grid)
+	gridSystem.playerOneMask = init_array(gridSystem.playerOneMask)
+	gridSystem.playerTwoMask = init_array(gridSystem.playerTwoMask)
 	randomize()
+	
+func init_array(array):
+	
+	for x in range(20):
+		var col = []
+		col.resize(20)
+		array.append(col)
+	
+	for x in range(20):
+		for y in range(20):
+			array[x][y] = 0
+	return array
 	
 func _on_StartGameButton_button_up():
 	get_node("StartGameButton").hide()
@@ -28,20 +70,18 @@ func _on_StartGameButton_button_up():
 	add_child(cursorSquare)
 	
 	cursorSquare.hide()
-	
-	gameStarted = true
 	#TODO: сделать это действие по анимации рандома
-	start_dice_animation_orSmth()
+	generate_new_square()
 	
 func _input(event):
-	if gameStarted == true:
+	if gameStage != GameStages.MENU:
 		if event is InputEventMouseButton:
 			if event.button_index == BUTTON_LEFT and not event.pressed:
 				_on_left_button_click(event.position)
 			if event.button_index == BUTTON_WHEEL_UP and event.pressed:
-				rotate_squareToPlace(-90)
+				rotate_square(currentSquareToPlace, -90)
 			if event.button_index == BUTTON_WHEEL_DOWN and event.pressed:
-				rotate_squareToPlace(90)
+				rotate_square(currentSquareToPlace, 90)
 		elif event is InputEventMouseMotion:
 			lastMousePosition = event.position
 			move_cursor(event.position)	
@@ -51,25 +91,26 @@ func _on_left_button_click(mouse_position):
 		showErrorOnPlacing()
 		return
 	else:
-		 place_squareToPlace(mouse_position)
+		 place_squareToPlace()
+		
+func _process(delta):
+	gameTime+=delta
 	
+	#if gameStage == GameStages.TESTING:
+		#testingForEnd()
+	
+	return false
+		
 func placingRules():
 	var answer: bool
 	answer = true
-	var area1 = get_PlayerSquares_node(0)
+	var area1 = get_PlayerSquares_node(0)#TODO: сделать единичную инициализацию
 	var area2 = get_PlayerSquares_node(1)
-	
-	#var area1Array = area1.get_children()
-	#var area2Array = area2.get_children()
 	
 	var area1ChildCount = area1.get_child_count()
 	var area2ChildCount = area2.get_child_count()
 	
-	#var squarePosition = currentSquareToPlace.rect_position
-	#var squareSize = currentSquareToPlace.get_node("Area2DSquare").get_node("square").rect_size
-	
 	if howManySquaresColliding != 0:
-		showErrorOnPlacing()
 		answer = false
 	
 	if whoPlays == 0:	
@@ -79,7 +120,6 @@ func placingRules():
 		else:
 			if bordersCollidingWithPlayer.player1 == 0:
 				answer = false
-			else: bordersCollidingWithPlayer.player1 = 0
 	
 	if whoPlays == 1:
 		if area2ChildCount == 0:
@@ -88,7 +128,6 @@ func placingRules():
 		else:
 			if bordersCollidingWithPlayer.player2 == 0:
 				answer = false
-			else: bordersCollidingWithPlayer.player2 = 0
 	
 	return answer
 	
@@ -102,8 +141,8 @@ func move_cursor(position):
 		cursorSquare.visible = true
 		cursorSquare.rect_position = position
 		
-		var x_pos = stepify(position.x-320, 52)+320
-		var y_pos = stepify(position.y, 52)+18
+		var x_pos = stepify(position.x-FIELD_START_POINT.x, 52)+FIELD_START_POINT.x
+		var y_pos = stepify(position.y+18, 52)+18
 	
 		var currentSquarePos = Vector2(x_pos, y_pos)
 		
@@ -112,46 +151,90 @@ func move_cursor(position):
 		cursorSquare.hide()
 		
 func start_dice_animation_orSmth():
+	print("GameStage = ", "dice animation")
 	hideCursorSquare = true
-	generate_new_square()
+	
+	pass #animation things
+	
+	hideCursorSquare = false
 	
 func generate_new_square():
 	var w = randi()%6 + 1
 	var h = randi()%6 + 1
-	currentSquareToPlace = get_square(w, h)
-	currentSquareToPlace.rect_position = lastMousePosition 
-	get_node("PlayerSquares").add_child(currentSquareToPlace)
-	hideCursorSquare = false
-	$beforeEndTimer.start()
+	gameStage = GameStages.GENERATING
+	print("GameStage = ", gameStage)
+	print("Generated new square: ", w, ":", h)
 	
-func place_squareToPlace(mouse_position):
+	currentSquareToPlace = get_square(w, h)
+	
+	hideCursorSquare = true
+	
+	testingForEndInit(w,h)
+	#$beforeEndTimer.start()
+	
+func place_squareToPlace():
 	#starts with clicking LMB
+	gameStage = GameStages.PLACING
+	print("GameStage = ", gameStage)
+	
 	currentSquareToPlace.modulate = COLOR_CLEAR
 	
+	var square = currentSquareToPlace.get_node("Area2DSquare/square")
+	
+	var size_x = square.rect_size.x
+	var size_y = square.rect_size.y
+	
+	var pos_x: int
+	var pos_y: int
+	pos_x = (currentSquareToPlace.rect_position.x-FIELD_START_POINT.x)/52
+	pos_y = (currentSquareToPlace.rect_position.y-FIELD_START_POINT.y)/52
+	
+	var width: int
+	var height: int
+	
+	width = (size_x - 4)/(56 - 4)
+	height = (size_y - 4)/(56 - 4)
+
+	if currentSquareToPlace.rect_rotation as int != 0:	
+		if rotationFixVector.x !=0 and rotationFixVector.y !=0:
+			pos_x -= width
+			pos_y -= height
+		elif rotationFixVector.x !=0:
+			pos_x -= height
+		elif rotationFixVector.y !=0:
+			pos_y -= width
+		
+		match currentSquareToPlace.rect_rotation as int:
+			90,-90, 270, -270:
+				var temp = width
+				width = height
+				height = temp
+			_:
+				pass
+
+	for x in range(pos_y, pos_y+height):
+		for y in range(pos_x, pos_x+width):
+			gridSystem.grid[x][y] = whoPlays+1
+	
 	get_node("PlayerSquares").remove_child(currentSquareToPlace)
-	
-	var x_pos = stepify(mouse_position.x-320, 52)+320
-	var y_pos = stepify(mouse_position.y, 52)+18
-	
-	var currentSquareToPlacePos = Vector2(x_pos, y_pos)
-	currentSquareToPlace.rect_position = currentSquareToPlacePos + rotationFixVector
-	
 	get_PlayerSquares_node(whoPlays).add_child(currentSquareToPlace)
 	
-	rotationFixVector = Vector2(0,0)
-	
 	change_team()
-	start_dice_animation_orSmth() #just creates new square using get_square()
-
-func rotate_squareToPlace(angle):
-	currentSquareToPlace.rect_position -= rotationFixVector
+	bordersCollidingWithPlayer.player1 = 0
+	bordersCollidingWithPlayer.player2 = 0
 	rotationFixVector = Vector2(0,0)
-	currentSquareToPlace.rect_rotation +=angle
-	if currentSquareToPlace.rect_rotation >=360 or currentSquareToPlace.rect_rotation <=-360:
-		currentSquareToPlace.rect_rotation = 0
+	generate_new_square()
+	
+
+func rotate_square(square, angle):
+	square.rect_position -= rotationFixVector
+	rotationFixVector = Vector2(0,0)
+	square.rect_rotation +=angle
+	if square.rect_rotation >=360 or square.rect_rotation <=-360:
+		square.rect_rotation = 0
 
 	var rotation: int 
-	rotation = currentSquareToPlace.rect_rotation
+	rotation = square.rect_rotation
 	match rotation:
 		-270, 90:
 			#x-=4
@@ -164,8 +247,8 @@ func rotate_squareToPlace(angle):
 			rotationFixVector.y = 4
 		_:
 			rotationFixVector = Vector2(0,0)
-	
-	currentSquareToPlace.rect_position += rotationFixVector
+
+	square.rect_position += rotationFixVector
 		
 func change_team():
 	if whoPlays == 0:
@@ -176,7 +259,6 @@ func change_team():
 		whoPlays = 0
 
 func get_square(width, height):
-		#get square of a sizes you need with team now plays
 	var squareScene
 	if whoPlays == 0:
 		squareScene = load("res://Scenes/SquarePlayerOne.tscn").instance()
@@ -215,11 +297,11 @@ func get_square(width, height):
 	collision.shape.extents = (sizeVector/2)-Vector2(4,4)
 	collision.position = (sizeVector/2)
 	
-	collisionForBorders.shape.extents = (sizeVector/2) + Vector2(10,10)
+	collisionForBorders.shape.extents = (sizeVector/2) #+ Vector2(4,4)
 	collisionForBorders.position = (sizeVector/2) 
 	squareScene.get_node("Area2DSquare").connect("area_exited", self, "_on_square_area_exited")
 	squareScene.get_node("Area2DSquare").connect("area_entered", self, "_on_square_area_entered")
-		
+
 	return squareScene
 
 func _on_square_area_entered(another_area):
@@ -227,66 +309,82 @@ func _on_square_area_entered(another_area):
 
 	if name_of_area == "Area2DSquare":
 		howManySquaresColliding+=1
-		
+		if PRINT_COLLIDERS_INFO:
+			print(gameTime as String+" P", whoPlays+1, "; ", another_area.get_name()+ " enters square area sized "
+		 + another_area.get_node("square").rect_size.x as String + ";" + another_area.get_node("square").rect_size.y as String)
 	
 func _on_square_area_exited(another_area):
 	var name_of_area = another_area.get_name()
 	
 	if name_of_area == "Area2DSquare":
 		howManySquaresColliding-=1
+		if PRINT_COLLIDERS_INFO:
+			print(gameTime as String+" P", whoPlays+1, "; ", another_area.get_name()+ " leaves square area")
 
 func _on_background_area_entered(another_area):
 	if another_area.get_name() == "Area2DSquare":
 		howManySquaresColliding+=1
-		print("P", whoPlays+1, "; ", another_area.get_name()+ " enters background")		
+		if PRINT_COLLIDERS_INFO:
+			print(gameTime as String+" P", whoPlays+1, "; ", another_area.get_name()+ " enters background")	
 	
 func _on_background_area_exited(another_area):
 	if another_area.get_name() == "Area2DSquare":
 		howManySquaresColliding-=1
-		print("P", whoPlays+1, "; ", another_area.get_name()+ " leaves background")	
+		if PRINT_COLLIDERS_INFO:
+			print(gameTime as String+" P", whoPlays+1, "; ", another_area.get_name()+ " leaves background")	
 	
 func _on_borderPlayer1_area_entered(another_area):
+	var a = another_area.get_name()
 	if another_area.get_name() == "Area2DforBordersPlayerOne":
 		bordersCollidingWithPlayer.player1 += 1
-		print("P", whoPlays+1, "; ", another_area.get_name()+ " enters borderPlayer1_area")	
+		if PRINT_COLLIDERS_INFO:
+			print(gameTime as String+" P", whoPlays+1, "; ", another_area.get_name()+ " enters borderPlayer1_area")	
+		#print(bordersCollidingWithPlayer)
 	
 func _on_borderPlayer1_area_exited(another_area):
 	if another_area.get_name() == "Area2DforBordersPlayerOne":
 		bordersCollidingWithPlayer.player1 -= 1
-		print("P", whoPlays+1, "; ", another_area.get_name()+ " leaves borderPlayer1_area")	
+		if PRINT_COLLIDERS_INFO:
+			print(gameTime as String+" P", whoPlays+1, "; ", another_area.get_name()+ " leaves borderPlayer1_area")	
+		#print(bordersCollidingWithPlayer)
 
 func _on_borderPlayer2_area_entered(another_area):
 	if another_area.get_name() == "Area2DforBordersPlayerTwo":
 		bordersCollidingWithPlayer.player2 += 1
-		print("P", whoPlays+1, "; ", another_area.get_name()+ " enters borderPlayer2_area")	
+		if PRINT_COLLIDERS_INFO:
+			print(gameTime as String+" P", whoPlays+1, "; ", another_area.get_name()+ " enters borderPlayer2_area")	
+		#print(bordersCollidingWithPlayer)
 
-	
 func _on_borderPlayer2_area_exited(another_area):
 	if another_area.get_name() == "Area2DforBordersPlayerTwo":
 		bordersCollidingWithPlayer.player2 -= 1
-		print("P", whoPlays+1, "; ", another_area.get_name()+ " leaves borderPlayer2_area")	
+		if PRINT_COLLIDERS_INFO:
+			print(gameTime as String+" P", whoPlays+1, "; ", another_area.get_name()+ " leaves borderPlayer2_area")	
+		#print(bordersCollidingWithPlayer)
 		
 func _on_playerOneStartPoints_area_entered(another_area):
 	if another_area.get_name() == "Area2DSquare":
 		collidingPlayersStartPoint = 1
-		print("P", whoPlays+1, "; ", another_area.get_name()+ " enters playerOneStartPoints_area")	
+		if PRINT_COLLIDERS_INFO:
+			print(gameTime as String+" P", whoPlays+1, "; ", another_area.get_name()+ " enters playerOneStartPoints_area")	
 
 func _on_playerOneStartPoints_area_exited(another_area):
 	if another_area.get_name() == "Area2DSquare":
 		collidingPlayersStartPoint = 0
-		print("P", whoPlays+1, "; ", another_area.get_name()+ " leaves playerOneStartPoints_area")	
-
+		if PRINT_COLLIDERS_INFO:
+			print(gameTime as String+" P", whoPlays+1, "; ", another_area.get_name()+ " leaves playerOneStartPoints_area")	
 
 func _on_playerTwoStartPoints_area_entered(another_area):
 	if another_area.get_name() == "Area2DSquare":
 		collidingPlayersStartPoint = 2
-		print("P", whoPlays+1, "; ", another_area.get_name()+ " enters playerTwoStartPoints_area")	
-
+		if PRINT_COLLIDERS_INFO:
+			print(gameTime as String+" P", whoPlays+1, "; ", another_area.get_name()+ " enters playerTwoStartPoints_area")	
 
 func _on_playerTwoStartPoints_area_exited(another_area):
 	if another_area.get_name() == "Area2DSquare":
 		collidingPlayersStartPoint = 0
-		print("P", whoPlays+1, "; ", another_area.get_name()+ " leaves playerTwoStartPoints_area")	
+		if PRINT_COLLIDERS_INFO:
+			print(gameTime as String+" P", whoPlays+1, "; ", another_area.get_name()+ " leaves playerTwoStartPoints_area")	
 
 func get_PlayerSquares_node(team):
 	if team == 0:
@@ -301,11 +399,164 @@ func _on_errorTimer_timeout():
 	else:
 		cursorSquare.modulate = COLOR_TEAM_TWO
 
-func _on_beforeEndTimer_timeout():
-	var scanning = ScanningForEnd.new()
-	var testingSquare = get_square(1, 1)
-	testingSquare.set_name("testingSquare")
-	add_child(testingSquare)
+func testingForEndInit(width, height):
 	
-	scanning.start()
-	pass # Replace with function body.
+	gameStage = GameStages.TESTING
+	print("GameStage = ", gameStage)
+
+	var area1ChildCount = get_PlayerSquares_node(0).get_child_count()
+	var area2ChildCount = get_PlayerSquares_node(1).get_child_count()
+#
+	for x in range(20):
+		print(gridSystem.grid[x])
+		
+	if area1ChildCount == 0 or area2ChildCount == 0 or testingForEnd(width, height) == false:
+		gameStage = GameStages.TESTED
+#		if testingObject.Square != null:
+#			testingObject.Square.free()
+		get_node("PlayerSquares").add_child(currentSquareToPlace)
+		currentSquareToPlace.rect_position = lastMousePosition
+		rotationFixVector = Vector2(0,0)
+		start_dice_animation_orSmth()
+	pass
+func testingForEnd(width, height):
+	var answer = true
+	var grid = gridSystem.grid
+	
+	var minSize = width
+	var maxSize = width
+	
+	if width > height:
+		maxSize = width
+		minSize = height
+	elif width < height:
+		maxSize = height
+		minSize = width
+		
+	for x in range(0, 20-minSize):
+		for y in range(0,20-maxSize):
+			if (x==0 and y==0) or (x == 19 and y==19):
+				continue
+			if grid[x][y] != 0:
+				continue
+			
+			for rotated in range(1):		
+				var isBreak = false
+				
+				var temp_width = width
+				var temp_height = height
+				
+				if rotated == 1:
+					temp_width = height
+					temp_height = width
+				
+				if (x+temp_width > 19 and y+temp_height > 19):
+					continue
+				for i in range(temp_width):
+					for j in range(temp_height):
+						if (grid[x+i][y+j] !=0):
+							isBreak = true
+							break
+						pass
+					if isBreak == true:
+						break
+					pass
+				
+				if isBreak == true:
+					answer = true
+					continue
+				else:
+					
+					#добавить проверку на наличие рядом других блоков того же 
+					#цвета по окончанию проверки	
+					
+					answer = false
+					return answer
+				pass
+			pass
+		pass
+
+#
+#	testingObject.Square = currentSquareToPlace.duplicate()
+#	testingObject.Square.set_name("testingSquare")
+#	var scene = testingObject.Square
+#
+#	testingObject.SquareSize = scene.get_node("Area2DSquare").get_node("square").rect_size
+#	var position
+#	if whoPlays == 0:
+#		position = FIELD_START_POINT + Vector2(52,0)
+#	else:
+#		position = FIELD_BORDERS - Vector2(52,52)
+#
+#	testingObject.Square.rect_position = position
+#
+#	if testingObject.SquareSize.x > testingObject.SquareSize.y:
+#		testingObject.MinSize = testingObject.SquareSize.y
+#		testingObject.BiggerSize = testingObject.SquareSize.x
+#	else: 
+#		testingObject.MinSize = testingObject.SquareSize.x
+#		testingObject.BiggerSize = testingObject.SquareSize.y
+#
+#
+#	add_child(testingObject.Square)	
+#
+#	print(gameTime as String+" T ; testingSquare placed")
+#	return false
+#
+#func testingForEnd():
+#	var found = false #change to false by default
+#
+#	var area1 = get_PlayerSquares_node(0)
+#	var area2 = get_PlayerSquares_node(1)
+#
+#	var area1ChildCount = area1.get_child_count()
+#	var area2ChildCount = area2.get_child_count()
+#
+#	if area1ChildCount == 0 or area2ChildCount == 0:
+#		found = true
+#	if found != true:
+#		if placingRules() == true:
+#			found = true
+#		else:			
+#			if whoPlays == 0:
+#				var howCloseToBorder = testingObject.Square.rect_position.x + testingObject.MinSize
+#				if howCloseToBorder <= FIELD_BORDERS.x:
+#					if testingObject.Rotated == 0:
+#						rotate_square(testingObject.Square, -90)
+#						testingObject.Square.rect_position += Vector2(0, testingObject.SquareSize.x)
+#						testingObject.Rotated = 1
+#					else:
+#						testingObject.Rotated = 0
+#						rotate_square(testingObject.Square, 90)
+#						testingObject.Square.rect_position -= Vector2(0, testingObject.SquareSize.x)
+#						testingObject.Square.rect_position += Vector2(52, 0)
+#				else:
+#					testingObject.Square.rect_position.x = FIELD_START_POINT.x
+#					if (testingObject.Square.rect_position.y + testingObject.BiggerSize < FIELD_BORDERS.y):
+#						testingObject.Square.rect_position.y += 52
+#			else:
+#				if testingObject.Square.rect_position.x >= FIELD_START_POINT.x:
+#					if testingObject.Rotated == 0:
+#						rotate_square(testingObject.Square, -90)
+#						#testingObject.Square.rect_position -= Vector2(0, testingObject.SquareSize.x)
+#						testingObject.Rotated = 1
+#					else:
+#						testingObject.Rotated = 0
+#						rotate_square(testingObject.Square, 90)
+#						#testingObject.Square.rect_position += Vector2(0, testingObject.SquareSize.x)
+#						testingObject.Square.rect_position -= Vector2(52, 0)
+#				else:
+#					testingObject.Square.rect_position.x = FIELD_BORDERS.x
+#					testingObject.Square.rect_position.y -= 52
+#
+#	#нужно все же пропускать те кубы, в которые точно есть попадание	
+#	if found == true:
+#		gameStage = GameStages.TESTED
+#		if testingObject.Square != null:
+#			testingObject.Square.free()
+#		get_node("PlayerSquares").add_child(currentSquareToPlace)
+#		currentSquareToPlace.rect_position = lastMousePosition
+#		rotationFixVector = Vector2(0,0)
+#		start_dice_animation_orSmth()
+#
+#
